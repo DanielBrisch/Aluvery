6a8b08e95a1fa44c7c4b8f07281d5978f3cc378a
daniel.cibolli <danielbrisch157@gmail.com>
2024-07-09 08:31:06 -0300

    feat: adicionando parametros nos text e modificando alinhamento de itens

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -10,12 +10,17 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.aluvery.ui.theme.AluveryTheme
 import com.example.aluvery.ui.theme.Purple500
 import com.example.aluvery.ui.theme.Teal200
@@ -33,25 +38,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun ProductItem() {
-    Column(Modifier
-        .height(250.dp)
-        .width(200.dp)) {
+    Column(
+        Modifier
+            .height(250.dp)
+            .width(200.dp)) {
         Box(modifier = Modifier
             .height(100.dp)
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(
+                        Purple500, Teal200
+                    )
+                )
+            )
             .fillMaxWidth()
-            .background(brush = Brush.horizontalGradient(colors = listOf(
-                Purple500, Teal200
-            )))) {
+        ) {
             Image(painter = painterResource(
                 id = R.drawable.ic_launcher_background),
                 contentDescription = "imagem do produto",
-                Modifier.size(100.dp).clip(shape = CircleShape)
+                Modifier
+                    .size(100.dp)
+                    .offset(y = 50.dp)
+                    .clip(shape = CircleShape)
+                    .align(Alignment.BottomCenter)
                 )
         }
-        Text(text = "teste 1")
-        Text(text = "teste 2")
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(text = LoremIpsum(50).values.first(),
+            fontSize = 18.sp,
+            fontWeight = FontWeight(700),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(text = "R$ 14,99",
+            fontSize = 14.sp,
+            fontWeight = FontWeight(400))
     }
 }
