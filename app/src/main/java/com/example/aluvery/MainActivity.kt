449459f3a9940f047587e6fd4e40c2a9e89d64bb
daniel.cibolli <danielbrisch157@gmail.com>
2024-07-16 08:34:16 -0300

    feat: finalizando layout e começando a padronizar a estrutura do projeto

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -3,78 +3,112 @@ package com.example.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.aluvery.model.Product
+import com.example.aluvery.ui.components.ProductItem
 import com.example.aluvery.ui.theme.AluveryTheme
-import com.example.aluvery.ui.theme.Purple500
-import com.example.aluvery.ui.theme.Teal200
+import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AluveryTheme {
-                Surface {
-                    ProductItem()
-                }
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    AluveryTheme {
+        Surface {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .verticalScroll(
+                        rememberScrollState()
+                    ),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Spacer(Modifier)
+                ProductsSection()
+                ProductsSection()
+                ProductsSection()
+                Spacer(Modifier)
             }
         }
     }
 }
 
-@Preview
 @Composable
-fun ProductItem() {
-    Column(
-        Modifier
-            .height(250.dp)
-            .width(200.dp)) {
-        Box(modifier = Modifier
-            .height(100.dp)
-            .background(
-                brush = Brush.horizontalGradient(
-                    colors = listOf(
-                        Purple500, Teal200
+fun ProductsSection() {
+    Column {
+        Text(
+            text = "Promoções",
+            Modifier.padding(
+                start = 16.dp,
+                top = 16.dp,
+                end = 16.dp
+            ),
+            fontSize = 20.sp,
+            fontWeight = FontWeight(400)
+        )
+        Row(
+            Modifier
+                .padding(
+                    top = 8.dp,
+
                     )
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState()),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Spacer(Modifier)
+            ProductItem(
+                Product(
+                    name = "Hamburguer",
+                    price = BigDecimal("12.99"),
+                    image = R.drawable.hamburguer,
                 )
             )
-            .fillMaxWidth()
-        ) {
-            Image(painter = painterResource(
-                id = R.drawable.ic_launcher_background),
-                contentDescription = "imagem do produto",
-                Modifier
-                    .size(100.dp)
-                    .offset(y = 50.dp)
-                    .clip(shape = CircleShape)
-                    .align(Alignment.BottomCenter)
+            ProductItem(
+                Product(
+                    name = "Pizza",
+                    price = BigDecimal("19.99"),
+                    image = R.drawable.pizza,
                 )
+            )
+            ProductItem(
+                Product(
+                    name = "Fritas",
+                    price = BigDecimal("7.99"),
+                    image = R.drawable.fritas,
+                )
+            )
+            Spacer(Modifier)
         }
-        Spacer(modifier = Modifier.height(50.dp))
-        Text(text = LoremIpsum(50).values.first(),
-            fontSize = 18.sp,
-            fontWeight = FontWeight(700),
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis
-        )
-        Text(text = "R$ 14,99",
-            fontSize = 14.sp,
-            fontWeight = FontWeight(400))
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProductsSectionPreview() {
+    ProductsSection()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    App()
 }
