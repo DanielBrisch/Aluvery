75a6be53d74f2188705ca8deba53b92880dc99ef
daniel.cibolli <danielbrisch157@gmail.com>
2024-07-19 08:22:05 -0300

    feat: aleterações visuais e de projeto

--- app/src/main/java/com/example/aluvery/extensions/BigDecimalExtension.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aluvery.extensions
-
-import java.math.BigDecimal
-import java.text.NumberFormat
-import java.util.*
-
-fun BigDecimal.toBrazilianCurrency(): String {
-    return NumberFormat.getCurrencyInstance(Locale("pt", "br")).format(this)
-}
